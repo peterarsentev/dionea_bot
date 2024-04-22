ff9ea2fcf0a841de77e69c34d27d27122291cdbd
parsentev <parsentev@yandex.ru>
2024-04-22 16:08:21 +0300

    Fix header links

---
+++ src/test/kotlin/pro/dionea/SpamAnalysisEnglishTextTest.kt
@@ -0,0 +1,39 @@
+package pro.dionea
+
+import org.assertj.core.api.Assertions.*
+import org.junit.jupiter.api.Disabled
+import org.junit.jupiter.api.Test
+import pro.dionea.domain.Filter
+import pro.dionea.domain.KValue
+import pro.dionea.domain.Key
+import pro.dionea.repository.FilterFakeRepository
+import pro.dionea.repository.KValueFakeRepository
+import pro.dionea.repository.KeyFakeRepository
+import pro.dionea.service.FilterService
+import pro.dionea.service.KValueService
+import pro.dionea.service.KeyService
+import pro.dionea.service.SpamAnalysis
+
+@Disabled
+class SpamAnalysisEnglishTextTest {
+
+    @Test
+    fun whenEnglishTextIsSpamThenTrue() {
+        val filterRepository = FilterFakeRepository()
+        val filterService = FilterService(filterRepository)
+        val keyRepository = KeyFakeRepository()
+        val keyService = KeyService(keyRepository)
+        val kvalueRepository = KValueFakeRepository()
+        val kvalueService = KValueService(kvalueRepository)
+        val filter = filterRepository.save(Filter(1))
+        val keyJob = keyRepository.save(Key(1, filter))
+        kvalueRepository.save(KValue(1, keyJob, "trading"))
+        val keyMessage = keyRepository.save(Key(2, filter))
+        kvalueRepository.save(KValue(2, keyMessage, "interested"))
+        val text = ", I saw a post about your platform and I took a step to try if it’s going to work you changed my life financially \n" +
+                "Why not use your 5 to 6 hours to learn how  forxe trading market , work while waiting for your salary \n" +
+                "5 people are needed in this group to come and  learn forxes trading market online , to earn \$5,520 in 2 business days if interested ask how ⬇\uFE0F⬇\uFE0F\n" +
+                "CLICK ON LINK (http://t.me/+qXa2EeAOVUw0ZDY0)";
+        assertThat(SpamAnalysis(filterService, keyService, kvalueService).isSpam(text)).isTrue()
+    }
+}
