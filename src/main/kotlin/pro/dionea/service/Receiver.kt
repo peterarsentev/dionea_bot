c571972f3ebcd1bb3f4768e85062e9560ce21954
parsentev <parsentev@yandex.ru>
2024-05-09 14:01:02 +0300

    Исправить опечатки в тексте #18

--- src/main/kotlin/pro/dionea/service/Receiver.kt
+++ src/main/kotlin/pro/dionea/service/Receiver.kt
@@ -19,145 +19,136 @@ import pro.dionea.domain.*
 import java.sql.Timestamp
 
 @Service
-class Receiver(@Value("\${tg.name}") val name: String,
-               @Value("\${tg.token}") val token: String,
-               val analysis: SpamAnalysis,
-               val spamService: SpamService,
-               val voteService: VoteService,
-               val userService: UserService,
-               val encoding: PasswordEncoder,
-               val contactService: ContactService,
-               val chatService: ChatService)
-    : TelegramLongPollingBot() {
-
-    private fun deleteByVoteMessage(replyMessage: Message, targetMessage: Message) {
-        val name = replyMessage.from.userName ?: "unknown"
-        val spammer = contactService.findByName(name)
-            ?: contactService.add(
-                Contact().apply {
-                    tgUserId = replyMessage.from.id
-                    username = name
-                    firstName = replyMessage.from.firstName
-                    lastName = replyMessage.from.lastName  ?: ""
-                }
-            )
-        val spam = Spam().apply {
-            text = replyMessage.text
-            time = Timestamp(System.currentTimeMillis())
-            contact = spammer
-            chat = findChat(replyMessage)
-        }
-        spamService.add(spam)
-        execute(DeleteMessage(
-            replyMessage.chatId.toString(),
-            targetMessage.messageId
-        ))
-        execute(DeleteMessage(
-            replyMessage.chatId.toString(),
-            replyMessage.messageId
-        ))
-    }
+class Receiver(
+    @Value("\${tg.name}") val name: String,
+    @Value("\${tg.token}") val token: String,
+    val analysis: SpamAnalysis,
+    val spamService: SpamService,
+    val voteService: VoteService,
+    val userService: UserService,
+    val encoding: PasswordEncoder,
+    val contactService: ContactService,
+    val chatService: ChatService
+) : TelegramLongPollingBot() {
 
     override fun onUpdateReceived(update: Update) {
         if (update.hasCallbackQuery()) {
-            val replyMessage = update.callbackQuery.message.replyToMessage
-            val targetMessage = update.callbackQuery.message
-            val voteByContact = if (update.callbackQuery.data == "yes") Vote.YES else Vote.NO
-            voteService.save(Vote().apply {
-                chatId = replyMessage.chat.id
-                messageId = replyMessage.messageId.toLong()
-                userId = update.callbackQuery.from.id
-                vote = voteByContact
-            })
-            val votes = voteService.findByMessageId(replyMessage.messageId.toLong())
-            val votesYes = votes.count { it.vote == Vote.YES }
-            val votesNo = votes.size - votesYes
-            val updateVote = EditMessageReplyMarkup().apply {
-                chatId = replyMessage.chat.id.toString()
-                messageId = targetMessage.messageId
-            }
-            val markupInline = InlineKeyboardMarkup()
-            val rowsInline: MutableList<List<InlineKeyboardButton>> = ArrayList()
-            val rowInline: MutableList<InlineKeyboardButton> = ArrayList()
-            rowInline.add(InlineKeyboardButton().apply {
-                text = "Да: $votesYes"
-                callbackData = "yes"
-            })
-            rowInline.add(InlineKeyboardButton().apply {
-                text = "Нет: $votesNo"
-                callbackData = "no"
-            })
-            rowsInline.add(rowInline)
-            markupInline.keyboard = rowsInline
-            updateVote.replyMarkup = markupInline
-            execute(updateVote)
-            if (votesYes >= 3) {
-                deleteByVoteMessage(replyMessage, targetMessage)
-            }
+            handleCallbackQuery(update)
             return
         }
-        if (!(update.hasMessage() && update.message.text != null)) {
+
+        if (!update.hasMessage() || update.message.text == null) {
             return
         }
+
         val message = update.message
         val chatStatistics = findChat(message)
-        if (message.chat.type == "private") {
-            if (message.text.startsWith("/")) {
-                if ("/start" == message.text) {
-                    execute(
-                        SendMessage(message.chatId.toString(),
-                            "$name гибкий помощник для телеграмм групп:\n" +
-                                    "- анализирует и удаляет спам сообщения\n" +
-                                    "- поддерживает режим голосования за удаление сообщения или бан участника\n" +
-                                    "- позволяет настроить собственные стоп-фильтры через веб-интерфейс\n" +
-                                    "- публикация сообщений по расписанию\n\n" +
-                                    "Подробнее тут https://job4j.ru/dionea\n" +
-                                    "/reg - для регистрации пользователя"
-                        )
+
+        when {
+            message.chat.type == "private" && message.text.startsWith("/") -> handlePrivateCommand(message)
+            message.text.contains(name) && message.isReply -> handleSpamVoteRequest(message)
+            else -> handlePotentialSpam(message, chatStatistics)
+        }
+    }
+
+    private fun handleCallbackQuery(update: Update) {
+        val replyMessage = update.callbackQuery.message.replyToMessage ?: return
+        val targetMessage = update.callbackQuery.message
+        val voteByContact = if (update.callbackQuery.data == "yes") Vote.YES else Vote.NO
+        voteService.save(Vote().apply {
+            chatId = replyMessage.chat.id
+            messageId = replyMessage.messageId.toLong()
+            userId = update.callbackQuery.from.id
+            vote = voteByContact
+        })
+        val votes = voteService.findByMessageId(replyMessage.messageId.toLong())
+        val votesYes = votes.count { it.vote == Vote.YES }
+        val votesNo = votes.size - votesYes
+        val updateVote = EditMessageReplyMarkup().apply {
+            chatId = replyMessage.chat.id.toString()
+            messageId = targetMessage.messageId
+        }
+        val markupInline = InlineKeyboardMarkup()
+        val rowsInline: MutableList<List<InlineKeyboardButton>> = ArrayList()
+        val rowInline: MutableList<InlineKeyboardButton> = ArrayList()
+        rowInline.add(InlineKeyboardButton().apply {
+            text = "Да: $votesYes"
+            callbackData = "yes"
+        })
+        rowInline.add(InlineKeyboardButton().apply {
+            text = "Нет: $votesNo"
+            callbackData = "no"
+        })
+        rowsInline.add(rowInline)
+        markupInline.keyboard = rowsInline
+        updateVote.replyMarkup = markupInline
+        execute(updateVote)
+        if (votesYes >= 3) {
+            deleteByVoteMessage(replyMessage, targetMessage)
+        }
+    }
+
+    private fun handlePrivateCommand(message: Message) {
+        if (message.text.startsWith("/")) {
+            if ("/start" == message.text) {
+                execute(
+                    SendMessage(
+                        message.chatId.toString(),
+                        "$name гибкий помощник для телеграмм групп:\n" +
+                                "- анализирует и удаляет спам сообщения\n" +
+                                "- поддерживает режим голосования за удаление сообщения или бан участника\n" +
+                                "- позволяет настроить собственные стоп-фильтры через веб-интерфейс\n" +
+                                "- публикация сообщений по расписанию\n\n" +
+                                "Подробнее тут https://job4j.ru/dionea\n" +
+                                "/reg - для регистрации пользователя"
                     )
-                }
-                if ("/reg" == message.text) {
-                    val pwd = KeyGen(8).generate()
-                    val user = userService.add(
-                        User().apply {
-                            username = message.from.userName
-                            password = encoding.encode(pwd)
-                            enabled = true
-                        }
+                )
+            }
+            if ("/reg" == message.text) {
+                val pwd = KeyGen(8).generate()
+                val user = userService.add(
+                    User().apply {
+                        username = message.from.userName
+                        password = encoding.encode(pwd)
+                        enabled = true
+                    }
+                )
+                execute(
+                    SendMessage(
+                        message.chatId.toString(),
+                        "Доступ к сайту https://job4j.ru/dionea\n" +
+                                "Логин: ${user.username}\n" +
+                                "Пароль: $pwd"
                     )
-                    execute(
-                        SendMessage(message.chatId.toString(),
-                            "Доступ к сайту https://job4j.ru/dionea\n" +
-                                    "Логин: ${user.username}\n" +
-                                    "Пароль: $pwd"
-                        )
-                    )
-                }
+                )
             }
         }
-        if (message.text.contains(name) && message.isReply) {
-            val voteMessage = SendMessage().apply {
-                chatId = message.chatId.toString()
-                text = "Это спам? Проголосуйте за 3 раза."
-            }
-            val markupInline = InlineKeyboardMarkup()
-            val rowsInline: MutableList<List<InlineKeyboardButton>> = ArrayList()
-            val rowInline: MutableList<InlineKeyboardButton> = ArrayList()
-            rowInline.add(InlineKeyboardButton().apply {
-                text = "Да: 0"
-                callbackData = "yes"
-            })
-            rowInline.add(InlineKeyboardButton().apply {
-                text = "Нет: 0"
-                callbackData = "no"
-            })
-            rowsInline.add(rowInline)
-            markupInline.keyboard = rowsInline
-            voteMessage.replyMarkup = markupInline
-            voteMessage.replyToMessageId = message.replyToMessage.messageId
-            execute(voteMessage)
-            return
+    }
+
+    private fun handleSpamVoteRequest(message: Message) {
+        val voteMessage = SendMessage().apply {
+            chatId = message.chatId.toString()
+            text = "Это спам? Проголосуйте за 3 раза."
         }
+        val markupInline = InlineKeyboardMarkup()
+        val rowsInline: MutableList<List<InlineKeyboardButton>> = ArrayList()
+        val rowInline: MutableList<InlineKeyboardButton> = ArrayList()
+        rowInline.add(InlineKeyboardButton().apply {
+            text = "Да: 0"
+            callbackData = "yes"
+        })
+        rowInline.add(InlineKeyboardButton().apply {
+            text = "Нет: 0"
+            callbackData = "no"
+        })
+        rowsInline.add(rowInline)
+        markupInline.keyboard = rowsInline
+        voteMessage.replyMarkup = markupInline
+        voteMessage.replyToMessageId = message.replyToMessage.messageId
+        execute(voteMessage)
+    }
+
+    private fun handlePotentialSpam(message: Message, chatStatistics: Chat) {
         val spamReason = analysis.isSpam(message.text)
         if (spamReason.spam) {
             val name = message.from.userName ?: "unknown"
@@ -201,6 +192,34 @@ class Receiver(@Value("\${tg.name}") val name: String,
                 title = message.chat.title
             }
         )
+
+    private fun deleteByVoteMessage(replyMessage: Message, targetMessage: Message) {
+        val name = replyMessage.from.userName ?: "unknown"
+        val spammer = contactService.findByName(name)
+            ?: contactService.add(
+                Contact().apply {
+                    tgUserId = replyMessage.from.id
+                    username = name
+                    firstName = replyMessage.from.firstName
+                    lastName = replyMessage.from.lastName  ?: ""
+                }
+            )
+        val spam = Spam().apply {
+            text = replyMessage.text
+            time = Timestamp(System.currentTimeMillis())
+            contact = spammer
+            chat = findChat(replyMessage)
+        }
+        spamService.add(spam)
+        execute(DeleteMessage(
+            replyMessage.chatId.toString(),
+            targetMessage.messageId
+        ))
+        execute(DeleteMessage(
+            replyMessage.chatId.toString(),
+            replyMessage.messageId
+        ))
+    }
 
     override fun getBotToken(): String = token
 
